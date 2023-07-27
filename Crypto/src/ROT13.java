447f91caf106ad2abf5c4ad9137a8c49f9d816fc
maggie <snydermag@gmail.com>
2023-07-27 08:07:48 -0400

    Lab finished

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -4,7 +4,13 @@ import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
 
+    Character cs;
+
+    Character cf;
+
     ROT13(Character cs, Character cf) {
+        this.cs = cs;
+        this.cf = cf;
     }
 
     ROT13() {
@@ -12,21 +18,60 @@ public class ROT13  {
 
 
     public String crypt(String text) throws UnsupportedOperationException {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String crypt = "";
+        Character key = Character.toUpperCase(cf);
 
-        return "";
+        StringBuilder secureText = new StringBuilder();
+
+        // Build the crypt
+        crypt = rotate(alphabet, key);
+
+        for (int i = 0; i < text.length(); i++){
+            if (!Character.isLetter(text.charAt(i))){
+                secureText.append(text.charAt(i));
+            }
+            else if (!Character.isUpperCase(text.charAt(i))){
+                Character temp = Character.toUpperCase(text.charAt(i));
+                int alphabetIndex = alphabet.indexOf(temp);
+                int cryptIndex = alphabetIndex;
+
+                secureText.append(Character.toLowerCase(crypt.charAt(cryptIndex)));
+            }
+            else{
+                Character temp = text.charAt(i);
+                int alphabetIndex = alphabet.indexOf(temp);
+                int cryptIndex = alphabetIndex;
+
+                secureText.append(crypt.charAt(cryptIndex));
+            }
+        }
+
+        return secureText.toString();
     }
 
     public String encrypt(String text) {
-        return text;
+
+        return crypt(text);
     }
 
     public String decrypt(String text) {
-        return text;
+        return encrypt(text);
     }
 
     public static String rotate(String s, Character c) {
+        StringBuilder string = new StringBuilder();
+        int index = s.indexOf(c);
 
-        return "";
+        for (int i = 0; i < s.length(); i++){
+            if (index >= s.length()){
+                index = 0;
+            }
+            string.append(s.charAt(index));
+            index++;
+        }
+
+        return string.toString();
     }
 
 }
